d2e331f166c73c2be3eb95bab10ddf9bbc2d0745
Andrew Garetov <andrew1gar@yandex.ru>
2024-06-01 22:17:52 +0300

    #s1-1 Вынос BadRequest в отдельный файл

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"github.com/AndrXxX/go-metrics-collector/internal/handlers"
 	"github.com/AndrXxX/go-metrics-collector/internal/handlers/counter"
 	"github.com/AndrXxX/go-metrics-collector/internal/handlers/gauge"
 	"github.com/AndrXxX/go-metrics-collector/internal/repositories/memStorage"
@@ -14,11 +15,7 @@ func Run() error {
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/update/counter/{metric}/{value}", counter.Handler(&storage))
 	rtr.HandleFunc("/update/gauge/{metric}/{value}", gauge.Handler(&storage))
-	rtr.HandleFunc("/*", badRequest)
+	rtr.HandleFunc("/*", handlers.BadRequest)
 	muxServe.Handle("/", rtr)
 	return http.ListenAndServe(`:8080`, muxServe)
-}
-
-func badRequest(w http.ResponseWriter, _ *http.Request) {
-	http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 }
