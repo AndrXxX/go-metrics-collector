6d0b10f20e6e26ae6edb80cb533fe898fdcc09f7
Andrew Garetov <andrew1gar@yandex.ru>
2024-06-02 10:05:42 +0300

    #s1-1 Корректное прохождение тестов

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"github.com/AndrXxX/go-metrics-collector/internal/handlers"
 	"github.com/AndrXxX/go-metrics-collector/internal/handlers/counter"
 	"github.com/AndrXxX/go-metrics-collector/internal/handlers/gauge"
 	"github.com/AndrXxX/go-metrics-collector/internal/repositories/memstorage"
@@ -14,7 +15,8 @@ func Run() error {
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/update/counter/{metric}/{value}", counter.Handler(&storage))
 	rtr.HandleFunc("/update/gauge/{metric}/{value}", gauge.Handler(&storage))
-	rtr.HandleFunc("/*", http.NotFound)
+	rtr.HandleFunc("/update/{unknownType}/{metric}/{value}", handlers.BadRequest)
+	rtr.HandleFunc("/", http.NotFound)
 	muxServe.Handle("/", rtr)
 	return http.ListenAndServe(`:8080`, muxServe)
 }
