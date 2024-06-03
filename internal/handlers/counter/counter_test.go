f44319018aa5cd532a2b57b8760233c9e2292bb1
Andrew Garetov <andrew1gar@yandex.ru>
2024-06-03 20:03:16 +0300

    #s1-2 Мелкая правка counter_test.go

--- internal/handlers/counter/counter_test.go
+++ internal/handlers/counter/counter_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/AndrXxX/go-metrics-collector/internal/repositories/memstorage"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -55,10 +56,10 @@ func TestHandler(t *testing.T) {
 		{
 			name:    "StatusBadRequest test",
 			request: "/update/counter/test/dsff",
-			vars:    map[string]string{vars.METRIC: "test", vars.VALUE: "10"},
+			vars:    map[string]string{vars.METRIC: "test", vars.VALUE: "dsff"},
 			method:  http.MethodPost,
 			want: want{
-				statusCode:  http.StatusOK,
+				statusCode:  http.StatusBadRequest,
 				contentType: "text/plain",
 			},
 		},
@@ -75,6 +76,8 @@ func TestHandler(t *testing.T) {
 
 			assert.Equal(t, test.want.statusCode, result.StatusCode)
 			assert.Equal(t, test.want.contentType, result.Header.Get("Content-Type"))
+			err := result.Body.Close()
+			require.NoError(t, err)
 		})
 	}
 }
