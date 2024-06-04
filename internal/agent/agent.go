ac0ef9799fc3f54dbf8c044fe04481f49d49fa66
Andrew Garetov <andrew1gar@yandex.ru>
2024-06-04 14:31:10 +0300

    #s1-2 Передача client снаружи

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -5,6 +5,7 @@ import (
 	"github.com/AndrXxX/go-metrics-collector/internal/agent/executors"
 	"github.com/AndrXxX/go-metrics-collector/internal/agent/metrics"
 	"github.com/AndrXxX/go-metrics-collector/internal/agent/utils"
+	"net/http"
 	"time"
 )
 
@@ -13,7 +14,7 @@ func Run(config *config.Config) error {
 	scheduler := executors.NewIntervalScheduler()
 	scheduler.Add(executors.NewCollector(&config.Metrics), time.Duration(config.Intervals.PollInterval)*time.Second)
 
-	rs := utils.NewRequestSender(utils.NewMetricURLBuilder(config.Common.Host))
+	rs := utils.NewRequestSender(utils.NewMetricURLBuilder(config.Common.Host), http.DefaultClient)
 	scheduler.Add(executors.NewUploader(rs), time.Duration(config.Intervals.ReportInterval)*time.Second)
 
 	err := scheduler.Run(*m)
--- internal/agent/utils/request_sender.go
+++ internal/agent/utils/request_sender.go
@@ -1,18 +1,26 @@
 package utils
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
+
+type Client interface {
+	Post(url, contentType string, body io.Reader) (resp *http.Response, err error)
+}
 
 type RequestSender struct {
 	ub URLBuilder
+	c  Client
 }
 
-func NewRequestSender(ub URLBuilder) *RequestSender {
-	return &RequestSender{ub: ub}
+func NewRequestSender(ub URLBuilder, c Client) *RequestSender {
+	return &RequestSender{ub: ub, c: c}
 }
 
 func (s *RequestSender) Post(params URLParams, contentType string) error {
 	url := s.ub.BuildURL(params)
-	resp, err := http.Post(url, contentType, nil)
+	resp, err := s.c.Post(url, contentType, nil)
 	if err != nil {
 		return err
 	}
