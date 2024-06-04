7ae85931499746a5b88713fd2137dc387a20b4c2
Andrew Garetov <andrew1gar@yandex.ru>
2024-06-04 20:18:39 +0300

    #s1-2 Добавлено тестирование canExecute в scheduler_test.go

--- internal/agent/executors/scheduler_test.go
+++ internal/agent/executors/scheduler_test.go
@@ -46,3 +46,37 @@ func TestIntervalScheduler_Add(t *testing.T) {
 		})
 	}
 }
+
+func Test_canExecute(t *testing.T) {
+	tests := []struct {
+		name string
+		i    item
+		want bool
+	}{
+		{
+			name: "interval 2, item executed now",
+			i:    item{e: &testExecutor{}, interval: 2 * time.Second, lastExecuted: time.Now()},
+			want: false,
+		},
+		{
+			name: "interval 5, item executed 4 seconds ago",
+			i:    item{e: &testExecutor{}, interval: 5 * time.Second, lastExecuted: time.Now().Add(-4 * time.Second)},
+			want: false,
+		},
+		{
+			name: "interval 2, item executed 2 seconds ago",
+			i:    item{e: &testExecutor{}, interval: 2 * time.Second, lastExecuted: time.Now().Add(-2 * time.Second)},
+			want: true,
+		},
+		{
+			name: "interval 5, item executed 6 seconds ago",
+			i:    item{e: &testExecutor{}, interval: 5 * time.Second, lastExecuted: time.Now().Add(-6 * time.Second)},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, canExecute(tt.i), "canExecute(%v)", tt.i)
+		})
+	}
+}
