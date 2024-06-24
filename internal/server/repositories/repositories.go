416283e537ca79f9d6e077a9c4a42fe8dadef3ee
Andrew Garetov <andrew1gar@yandex.ru>
2024-06-24 23:40:48 +0300

    #s2-6 Удаление неиспользуемых фрагментов

--- internal/server/repositories/repositories.go
+++ internal/server/repositories/repositories.go
@@ -1,17 +1,5 @@
 package repositories
 
-type GaugeStorage interface {
-	SetGauge(metric string, value float64)
-	GetGauge(metric string) (value float64, err error)
-	GetGaugeAll() map[string]float64
-}
-
-type CounterStorage interface {
-	SetCounter(metric string, value int64)
-	GetCounter(metric string) (value int64, err error)
-	GetCounterAll() map[string]int64
-}
-
 type Storage[T any] interface {
 	Insert(metric string, value T)
 	Get(metric string) (value T, ok bool)
