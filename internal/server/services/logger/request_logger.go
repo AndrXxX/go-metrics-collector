426f80a4bf48980b26a120427c6856f487f3e5d1
Andrew Garetov <andrew1gar@yandex.ru>
2024-06-24 18:56:57 +0300

    #s2-6 Добавлен Logger

---
+++ internal/server/services/logger/request_logger.go
@@ -0,0 +1,35 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"net/http"
+	"time"
+)
+
+func RequestLogger(h http.Handler) http.HandlerFunc {
+	logFn := func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+		lw := loggingResponseWriter{
+			ResponseWriter: w,
+			responseData:   responseData,
+		}
+		h.ServeHTTP(&lw, r)
+
+		duration := time.Since(start)
+
+		Log.Info(
+			"got incoming HTTP request",
+			zap.String("uri", r.RequestURI),
+			zap.String("method", r.Method),
+			zap.Int("status", responseData.status),
+			zap.Duration("duration", duration),
+			zap.Int("size", responseData.size),
+		)
+	}
+	return logFn
+}
