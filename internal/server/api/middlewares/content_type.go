1a0e43c8f2dcf325ad15838f114699f417a95b1a
Andrew Garetov <andrew1gar@yandex.ru>
2024-06-27 13:36:14 +0300

    #s2-6 Добавлена middleware SetContentType

---
+++ internal/server/api/middlewares/content_type.go
@@ -0,0 +1,18 @@
+package middlewares
+
+import (
+	"net/http"
+)
+
+type contentType struct {
+	ct string
+}
+
+func (m *contentType) Handle(w http.ResponseWriter, r *http.Request) (ok bool) {
+	w.Header().Set("Content-Type", m.ct)
+	return true
+}
+
+func SetContentType(ct string) *contentType {
+	return &contentType{ct}
+}
