7bcf5a701208803af2eb06056a98c505772fef63
Andrew Garetov <andrew1gar@yandex.ru>
2024-06-28 11:08:44 +0300

    #s2-6 Использование log.Fatal вместо panic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,16 +4,17 @@ import (
 	"github.com/AndrXxX/go-metrics-collector/internal/server"
 	"github.com/AndrXxX/go-metrics-collector/internal/server/config"
 	"github.com/AndrXxX/go-metrics-collector/internal/server/services/logger"
+	"log"
 )
 
 func main() {
 	settings := config.NewConfig()
 	if err := logger.Initialize(settings.LogLevel); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	parseFlags(settings)
 	parseEnv(settings)
 	if err := server.Run(settings); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
