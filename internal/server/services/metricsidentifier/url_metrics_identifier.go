6057aeb1b92e50d1590155d9e345fd69f62cb014
Andrew Garetov <andrew1gar@yandex.ru>
2024-07-01 18:08:50 +0300

    #s2-7 Заполнение метрики из переданного значения

--- internal/server/services/metricsidentifier/url_metrics_identifier.go
+++ internal/server/services/metricsidentifier/url_metrics_identifier.go
@@ -1,8 +1,10 @@
 package metricsidentifier
 
 import (
+	"fmt"
 	"github.com/AndrXxX/go-metrics-collector/internal/enums/vars"
 	"github.com/AndrXxX/go-metrics-collector/internal/server/models"
+	"github.com/AndrXxX/go-metrics-collector/internal/server/services/metricsvaluesetter"
 	"github.com/go-chi/chi/v5"
 	"net/http"
 )
@@ -11,10 +13,20 @@ type urlMetricsIdentifier struct {
 }
 
 func (i *urlMetricsIdentifier) Process(r *http.Request) (*models.Metrics, error) {
-	return &models.Metrics{
+	m := models.Metrics{
 		ID:    chi.URLParam(r, vars.Metric),
 		MType: chi.URLParam(r, vars.MetricType),
-	}, nil
+	}
+	value := chi.URLParam(r, vars.Value)
+	if value == "" {
+		return &m, nil
+	}
+	setter := metricsvaluesetter.Factory().SetterByType(m.MType)
+	if setter == nil {
+		return nil, fmt.Errorf("unknown metrics type %q", m.MType)
+	}
+	err := setter.Set(&m, value)
+	return &m, err
 }
 
 func NewURLIdentifier() *urlMetricsIdentifier {
