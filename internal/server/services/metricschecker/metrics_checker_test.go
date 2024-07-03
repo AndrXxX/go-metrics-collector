e68bd27fe56af7bf802250de8c0d2f113d9c4cd6
Andrew Garetov <andrew1gar@yandex.ru>
2024-07-03 21:08:11 +0300

    #s2-9 Добавлен metrics_checker_test

---
+++ internal/server/services/metricschecker/metrics_checker_test.go
@@ -0,0 +1,58 @@
+package metricschecker
+
+import (
+	"github.com/AndrXxX/go-metrics-collector/internal/enums/metrics"
+	"github.com/AndrXxX/go-metrics-collector/internal/server/models"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name string
+		want *metricsChecker
+	}{
+		{
+			name: "OK Test",
+			want: &metricsChecker{[]string{metrics.Counter, metrics.Gauge}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, New())
+		})
+	}
+}
+
+func TestMetricsCheckerIsValid(t *testing.T) {
+	tests := []struct {
+		name string
+		mc   *metricsChecker
+		m    *models.Metrics
+		want bool
+	}{
+		{
+			name: "Valid Counter Test",
+			mc:   &metricsChecker{[]string{metrics.Counter, metrics.Gauge}},
+			m:    &models.Metrics{MType: metrics.Counter},
+			want: true,
+		},
+		{
+			name: "Valid Gauge Test",
+			mc:   &metricsChecker{[]string{metrics.Counter, metrics.Gauge}},
+			m:    &models.Metrics{MType: metrics.Counter},
+			want: true,
+		},
+		{
+			name: "Not valid Test",
+			mc:   &metricsChecker{[]string{metrics.Counter, metrics.Gauge}},
+			m:    &models.Metrics{MType: "not valid"},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, tt.mc.IsValid(tt.m))
+		})
+	}
+}
