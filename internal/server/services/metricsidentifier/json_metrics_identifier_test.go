5421b72eb5561079b956e542a835dc150c7354f4
Andrew Garetov <andrew1gar@yandex.ru>
2024-07-03 20:46:42 +0300

    #s2-9 Добавлены тесты для json_metrics_identifier

---
+++ internal/server/services/metricsidentifier/json_metrics_identifier_test.go
@@ -0,0 +1,75 @@
+package metricsidentifier
+
+import (
+	"github.com/AndrXxX/go-metrics-collector/internal/enums/metrics"
+	"github.com/AndrXxX/go-metrics-collector/internal/server/models"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestNewJSONIdentifier(t *testing.T) {
+	tests := []struct {
+		name string
+		want *jsonMetricsIdentifier
+	}{
+		{
+			name: "Test OK NewJSONIdentifier",
+			want: &jsonMetricsIdentifier{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, NewJSONIdentifier())
+		})
+	}
+}
+
+func TestJSONMetricsIdentifierProcess(t *testing.T) {
+	type modelValue struct {
+		Value float64
+		Delta int64
+	}
+	tests := []struct {
+		name       string
+		body       string
+		modelValue *modelValue
+		want       *models.Metrics
+		wantErr    bool
+	}{
+		{
+			name:       "OK Counter test",
+			body:       "{\"id\":\"test\",\"type\":\"counter\",\"delta\":10,\"value\":null}",
+			modelValue: &modelValue{Delta: 10},
+			want:       &models.Metrics{ID: "test", MType: metrics.Counter},
+			wantErr:    false,
+		},
+		{
+			name:       "OK Gauge test",
+			body:       "{\"id\":\"test\",\"type\":\"gauge\",\"delta\":null,\"value\":10.1}",
+			modelValue: &modelValue{Value: 10.1},
+			want:       &models.Metrics{ID: "test", MType: metrics.Gauge},
+			wantErr:    false,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			i := &jsonMetricsIdentifier{}
+			request := httptest.NewRequest("", "/test", strings.NewReader(test.body))
+			m, err := i.Process(request)
+			require.Equal(t, test.wantErr, err != nil)
+			if !test.wantErr {
+				assert.Equal(t, test.want.ID, m.ID)
+				assert.Equal(t, test.want.MType, m.MType)
+				if test.modelValue != nil && test.modelValue.Delta != 0 {
+					assert.Equal(t, test.modelValue.Delta, *m.Delta)
+				}
+				if test.modelValue != nil && test.modelValue.Value != 0 {
+					assert.Equal(t, test.modelValue.Value, *m.Value)
+				}
+			}
+		})
+	}
+}
