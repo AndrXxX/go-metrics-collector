54a2c84e539dba4445fcdde2561d1e101f8b533f
Andrew Garetov <andrew1gar@yandex.ru>
2024-07-07 17:52:54 +0300

    #s2-9 Добавлена валидация struct tags для сервера

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/AndrXxX/go-metrics-collector/internal/server"
 	"github.com/AndrXxX/go-metrics-collector/internal/server/config"
 	"github.com/AndrXxX/go-metrics-collector/internal/services/logger"
+	"github.com/asaskevich/govalidator"
 	"log"
 )
 
@@ -14,6 +15,9 @@ func main() {
 	}
 	parseFlags(settings)
 	parseEnv(settings)
+	if _, err := govalidator.ValidateStruct(c.Intervals); err != nil {
+		log.Fatal(err)
+	}
 	if err := server.Run(settings); err != nil {
 		log.Fatal(err)
 	}
--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -1,9 +1,9 @@
 package config
 
 type Config struct {
-	Host            string
-	LogLevel        string
-	StoreInterval   int
-	FileStoragePath string
+	Host            string `valid:"minstringlength(3)"`
+	LogLevel        string `valid:"in(debug|info|warn|error|fatal)"`
+	StoreInterval   int    `valid:"range(1|999)"`
+	FileStoragePath string `valid:"minstringlength(3)"`
 	Restore         bool
 }
