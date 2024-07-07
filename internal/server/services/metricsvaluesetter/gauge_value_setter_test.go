8d0eb52a462f08d2de7095cb0571264efaf2dc82
Andrew Garetov <andrew1gar@yandex.ru>
2024-07-07 16:10:04 +0300

    #s2-9 Добавлен gauge_value_setter_test

---
+++ internal/server/services/metricsvaluesetter/gauge_value_setter_test.go
@@ -0,0 +1,53 @@
+package metricsvaluesetter
+
+import (
+	"github.com/AndrXxX/go-metrics-collector/internal/server/models"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGaugeValueSetterSet(t *testing.T) {
+	type args struct {
+		m     *models.Metrics
+		value string
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantValue float64
+		wantErr   bool
+	}{
+		{
+			name:      "Test OK with 10.1",
+			args:      args{m: &models.Metrics{}, value: "10.1"},
+			wantValue: 10.1,
+			wantErr:   false,
+		},
+		{
+			name:      "Test OK with 10",
+			args:      args{m: &models.Metrics{}, value: "10"},
+			wantValue: 10,
+			wantErr:   false,
+		},
+		{
+			name:    "Test Error with aaa",
+			args:    args{m: &models.Metrics{}, value: "aaa"},
+			wantErr: true,
+		},
+		{
+			name:    "Test Error with empty string",
+			args:    args{m: &models.Metrics{}, value: ""},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			setter := &gaugeValueSetter{}
+			err := setter.Set(tt.args.m, tt.args.value)
+			assert.Equal(t, tt.wantErr, err != nil)
+			if !tt.wantErr {
+				assert.Equal(t, tt.wantValue, *tt.args.m.Value)
+			}
+		})
+	}
+}
