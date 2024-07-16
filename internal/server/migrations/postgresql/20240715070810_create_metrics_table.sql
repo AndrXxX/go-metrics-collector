4575684281de840240c1d54272cbcc19ba91b448
Andrew Garetov <andrew1gar@yandex.ru>
2024-07-16 21:56:55 +0300

    #s3-13 Правка миграции

--- internal/server/migrations/postgresql/20240715070810_create_metrics_table.sql
+++ internal/server/migrations/postgresql/20240715070810_create_metrics_table.sql
@@ -1,10 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS metrics (
-    id SERIAL PRIMARY KEY,
     name text,
     type text,
-    delta int,
+    delta big int,
     value double precision
 );
 -- +goose StatementEnd
