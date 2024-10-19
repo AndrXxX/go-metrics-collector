6cbb461158946dcbdc2c790271f2ddc33c075fad
Andrew Garetov <andrew1gar@yandex.ru>
2024-10-19 19:15:01 +0300

    #s7-18 Добавлена документация для metrics_checker

--- internal/server/services/metricschecker/metrics_checker.go
+++ internal/server/services/metricschecker/metrics_checker.go
@@ -9,11 +9,13 @@ type metricsChecker struct {
 	validTypes map[string]struct{}
 }
 
+// IsValid проверяет валидность типа метрики
 func (c *metricsChecker) IsValid(m *models.Metrics) bool {
 	_, ok := c.validTypes[m.MType]
 	return ok
 }
 
+// New возвращает сервис metricsChecker для проверки типов метрик
 func New() *metricsChecker {
 	return &metricsChecker{
 		validTypes: map[string]struct{}{
