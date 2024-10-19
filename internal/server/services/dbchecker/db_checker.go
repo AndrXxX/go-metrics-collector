1ee84ec27d7c0cba395b32a6f8c5275824e5a0b9
Andrew Garetov <andrew1gar@yandex.ru>
2024-10-19 19:09:44 +0300

    #s7-18 Добавлена документация для db_checker

--- internal/server/services/dbchecker/db_checker.go
+++ internal/server/services/dbchecker/db_checker.go
@@ -10,10 +10,12 @@ type dbChecker struct {
 	db *sql.DB
 }
 
+// New возвращает сервис dbChecker для проверки соединения с базой данных
 func New(db *sql.DB) *dbChecker {
 	return &dbChecker{db}
 }
 
+// Check Проверяет соединение с базой данных
 func (c *dbChecker) Check(ctx context.Context) error {
 	if c.db == nil {
 		return fmt.Errorf("db is not initialized")
