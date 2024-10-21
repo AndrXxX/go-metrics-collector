19e7b88095b4af5b434fbd30d7f16a320b0cb83c
Andrew Garetov <andrew1gar@yandex.ru>
2024-10-21 18:34:30 +0300

    #s7-18 Добавлена документация для пакета mocks

--- internal/mocks/mock_client.go
+++ internal/mocks/mock_client.go
@@ -5,15 +5,18 @@ import (
 	"net/http"
 )
 
+// MockClient Mock клиент для тестирования
 type MockClient struct {
 	PostFunc func(url, contentType string, body io.Reader) (*http.Response, error)
 	DoFunc   func(req *http.Request) (*http.Response, error)
 }
 
+// Post отправляет запрос методом Post
 func (m *MockClient) Post(url, contentType string, body io.Reader) (*http.Response, error) {
 	return m.PostFunc(url, contentType, body)
 }
 
+// Do отправляет запрос
 func (m *MockClient) Do(req *http.Request) (*http.Response, error) {
 	return m.DoFunc(req)
 }
