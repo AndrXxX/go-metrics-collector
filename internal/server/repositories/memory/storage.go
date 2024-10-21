f349c730a6e854919f66c8d4cfc9228c7e2af91c
Andrew Garetov <andrew1gar@yandex.ru>
2024-10-21 19:20:37 +0300

    #s7-18 Добавлена документация для memory

--- internal/server/repositories/memory/storage.go
+++ internal/server/repositories/memory/storage.go
@@ -9,14 +9,17 @@ type storage[T any] struct {
 	m sync.Map
 }
 
+// New возвращает хранилище метрик в оперативной памяти
 func New[T any]() storage[T] {
 	return storage[T]{}
 }
 
+// Insert вставляет запись
 func (s *storage[T]) Insert(_ context.Context, name string, value T) {
 	s.m.Store(name, value)
 }
 
+// Get извлекает запись
 func (s *storage[T]) Get(_ context.Context, name string) (value T, ok bool) {
 	val, found := s.m.Load(name)
 	if val, ok := val.(T); ok {
@@ -26,6 +29,7 @@ func (s *storage[T]) Get(_ context.Context, name string) (value T, ok bool) {
 	return zero, found
 }
 
+// All извлекает все записи
 func (s *storage[T]) All(_ context.Context) map[string]T {
 	result := make(map[string]T)
 	s.m.Range(func(k, v interface{}) bool {
@@ -35,6 +39,7 @@ func (s *storage[T]) All(_ context.Context) map[string]T {
 	return result
 }
 
+// Delete удаляет запись
 func (s *storage[T]) Delete(_ context.Context, name string) (ok bool) {
 	s.m.Delete(name)
 	return true
