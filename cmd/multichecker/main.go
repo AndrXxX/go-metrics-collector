7f57fdebb68e529ceebf2c53f951c9d49bf0e966
Andrew Garetov <andrew1gar@yandex.ru>
2024-10-30 23:32:38 +0300

    #s8-19 Добавлен staticcheck в multichecker

--- cmd/multichecker/main.go
+++ cmd/multichecker/main.go
@@ -1,23 +1,63 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"regexp"
+
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/multichecker"
 	"golang.org/x/tools/go/analysis/passes/printf"
 	"golang.org/x/tools/go/analysis/passes/shadow"
 	"golang.org/x/tools/go/analysis/passes/shift"
 	"golang.org/x/tools/go/analysis/passes/structtag"
+	"honnef.co/go/tools/staticcheck"
+
+	"github.com/AndrXxX/go-metrics-collector/internal/multichecker/config"
+	"github.com/AndrXxX/go-metrics-collector/internal/multichecker/services/configprovider"
 )
 
 func main() {
-	checks := []*analysis.Analyzer{
+	c, err := configprovider.New().Fetch()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var checks []*analysis.Analyzer
+	checks = append(checks, getAnalysisChecks()...)
+
+	staticChecks, err := getStaticChecks(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	checks = append(checks, staticChecks...)
+
+	multichecker.Main(
+		checks...,
+	)
+}
+
+func getAnalysisChecks() []*analysis.Analyzer {
+	return []*analysis.Analyzer{
 		printf.Analyzer,
 		shadow.Analyzer,
 		shift.Analyzer,
 		structtag.Analyzer,
 	}
+}
 
-	multichecker.Main(
-		checks...,
-	)
+func getStaticChecks(c *config.Config) ([]*analysis.Analyzer, error) {
+	var checks []*analysis.Analyzer
+	for _, v := range staticcheck.Analyzers {
+		for _, check := range c.StaticChecks {
+			matched, err := regexp.MatchString(check, v.Analyzer.Name)
+			if err != nil {
+				return nil, fmt.Errorf("failed to match static check '%s': %v", v.Analyzer.Name, err)
+			}
+			if matched {
+				checks = append(checks, v.Analyzer)
+			}
+		}
+	}
+	return checks, nil
 }
--- go.mod
+++ go.mod
@@ -11,10 +11,13 @@ require (
 	github.com/shirou/gopsutil/v4 v4.24.9
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/zap v1.27.0
+	golang.org/x/tools v0.22.0
+	honnef.co/go/tools v0.5.1
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/ebitengine/purego v0.8.0 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
@@ -31,6 +34,8 @@ require (
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20231108232855-2478ac86f678 // indirect
+	golang.org/x/mod v0.18.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
