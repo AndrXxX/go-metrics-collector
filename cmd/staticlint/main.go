f49f84c3061f7847a69f49de431589f578995ec1
Andrew Garetov <andrew1gar@yandex.ru>
2024-10-31 21:56:37 +0300

    #s8-19 Правка документации

--- cmd/staticlint/main.go
+++ cmd/staticlint/main.go
@@ -1,3 +1,27 @@
+// # multichecker, состоящий из анализаторов:
+//
+// 1. Go анализаторы:
+//   - printf.Analyzer
+//   - shadow.Analyzer
+//   - shift.Analyzer
+//   - structtag.Analyzer
+//
+// 2. Дополнительные анализаторы:
+//   - testifylint анализатор тестов https://github.com/Antonboom/testifylint
+//   - errcheck анализатор необработанных ошибок https://github.com/kisielk/errcheck
+//   - OSExitAnalyzer анализатор использования вызово os.Exit() в функции main пакета main
+//
+// 3. Анализаторы staticcheck https://staticcheck.dev/
+//
+// По умолчанию включены анализаторы серии SA***, ST*** и QF***
+//
+// Можно указать другой набор анализаторов staticcheck:
+//   - с помощью переменных env, например: STATIC_ANALYZERS=SA1000,SA1032,SA4004,QF.*,SA4005
+//   - с помощью аргументов командной строки, например: -sa=SA1000,SA1032,SA4004,QF.*,SA4005
+//
+// Можно также исключить набор анализаторов staticcheck:
+//   - с помощью переменных env, например: EXCLUDE_STATIC_ANALYZERS=SA1000,SA1032,SA4004,QF.*,SA4005
+//   - с помощью аргументов командной строки, например: -esa=SA1000,SA1032,SA4004,QF.*,SA4005
 package main
 
 import (
@@ -11,19 +35,6 @@ import (
 	"github.com/AndrXxX/go-metrics-collector/internal/staticlint/services/flagsparser"
 )
 
-// multichecker
-// Комплексный анализатор, состоящий из анализаторов:
-// 1. golang анализаторы: printf.Analyzer, shadow.Analyzer, shift.Analyzer, structtag.Analyzer,
-// 2. дополнительные анализаторы:
-//   - testifylint анализатор тестов https://github.com/Antonboom/testifylint
-//   - errcheck анализатор необработанных ошибок https://github.com/kisielk/errcheck
-//   - OSExitAnalyzer анализатор использования вызово os.Exit() в функции main пакета main
-//
-// 3. анализаторы staticcheck https://staticcheck.dev/ - по умолчанию включены:
-// По умолчанию включены анализаторы серии SA***, ST*** и QF***
-// Можно указать другой набор анализаторов staticcheck:
-//   - с помощью переменных env, например: STATIC_ANALYZERS=SA1000,SA1032,SA4004,QF.*,SA4005
-//   - с помощью аргументов командной строки, например: -sa=SA1000,SA1032,SA4004,QF.*,SA4005
 func main() {
 	c, err := configprovider.New(envparser.EnvParser{}, flagsparser.FlagsParser{}).Fetch()
 	if err != nil {
