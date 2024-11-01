d170964960d717704ca207bddf418b7173fca1b9
Andrew Garetov <andrew1gar@yandex.ru>
2024-11-01 21:56:11 +0300

    #s8-19 Вывод информации о текущем билде в агенте

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os/signal"
 	"syscall"
+
+	"go.uber.org/zap"
 
 	"github.com/AndrXxX/go-metrics-collector/internal/agent"
 	"github.com/AndrXxX/go-metrics-collector/internal/agent/services/configprovider"
 	"github.com/AndrXxX/go-metrics-collector/internal/agent/services/envparser"
 	"github.com/AndrXxX/go-metrics-collector/internal/agent/services/flagsparser"
+	"github.com/AndrXxX/go-metrics-collector/internal/services/buildformatter"
 	"github.com/AndrXxX/go-metrics-collector/internal/services/logger"
 )
+
+var buildVersion string
+var buildDate string
+var buildCommit string
 
 func main() {
 	c, err := configprovider.New(flagsparser.New(), envparser.New()).Fetch()
@@ -23,6 +31,18 @@ func main() {
 	}
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
+
+	formatter := buildformatter.BuildFormatter{
+		Version: buildVersion,
+		Date:    buildDate,
+		Commit:  buildCommit,
+	}
+	if str, fErr := formatter.Format(); fErr != nil {
+		logger.Log.Error("Failed to print build info", zap.Error(fErr))
+	} else {
+		fmt.Println(str)
+	}
+
 	if err := agent.Run(ctx, c); err != nil {
 		log.Fatal(err)
 	}
