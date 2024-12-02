86bcd3099fbd0ca889065dad782235728209d206
Andrew Garetov <andrew1gar@yandex.ru>
2024-12-02 18:03:58 +0300

    #s10-25 Добавлен GRPCMetricsUpdater

---
+++ internal/agent/services/grpc/grpc_metrics_updater.go
@@ -0,0 +1,42 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
+
+	pb "github.com/AndrXxX/go-metrics-collector/internal/proto"
+)
+
+type metricsUpdater struct {
+	host string
+}
+
+func NewGRPCMetricsUpdater(host string) *metricsUpdater {
+	return &metricsUpdater{host: host}
+}
+
+func (u metricsUpdater) Update(ctx context.Context, list []*pb.Metric) error {
+	// устанавливаем соединение с сервером
+	conn, err := grpc.NewClient(u.host)
+	if err != nil {
+		return fmt.Errorf("grpc connection error: %w", err)
+	}
+	defer func(conn *grpc.ClientConn) {
+		_ = conn.Close()
+	}(conn)
+
+	c := pb.NewMetricsClient(conn)
+	req := &pb.UpdateMetricsRequest{Metrics: list}
+	_, err = c.UpdateMetrics(ctx, req)
+	if err != nil {
+		if e, ok := status.FromError(err); ok {
+			return fmt.Errorf("grpc response error: %s %w", e.Code(), e.Err())
+		} else {
+			return fmt.Errorf("grpc response error: %w", e.Err())
+		}
+	}
+	return nil
+}
