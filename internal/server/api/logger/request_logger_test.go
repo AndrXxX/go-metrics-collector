b0d3508a448c96f445b6b4bc578dd0ed3ed8122d
Andrew Garetov <andrew1gar@yandex.ru>
2024-12-05 17:43:40 +0300

    #s10-25 Добавлен тест Test_requestLogger_Handler

---
+++ internal/server/api/logger/request_logger_test.go
@@ -0,0 +1,42 @@
+package logger
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_requestLogger_Handler(t *testing.T) {
+	tests := []struct {
+		name     string
+		next     http.Handler
+		wantCode int
+	}{
+		{
+			name: "test StatusOK",
+			next: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+			}),
+			wantCode: http.StatusOK,
+		},
+		{
+			name: "test StatusForbidden",
+			next: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusForbidden)
+			}),
+			wantCode: http.StatusForbidden,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := New().Handler(tt.next)
+			request := httptest.NewRequest("", "/test", nil)
+			w := httptest.NewRecorder()
+			h.ServeHTTP(w, request)
+			result := w.Result()
+			assert.Equal(t, tt.wantCode, result.StatusCode)
+		})
+	}
+}
