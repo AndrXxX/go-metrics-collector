870bd29eb9321654cf56f8aa7cd50f89d2917d18
Andrew Garetov <andrew1gar@yandex.ru>
2024-12-05 15:07:17 +0300

    #s10-25 Поддержка RSA для клиента grpc

--- internal/agent/with_grpc_metrics_uploader.go
+++ internal/agent/with_grpc_metrics_uploader.go
@@ -1,13 +1,18 @@
 package agent
 
 import (
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/encoding/gzip"
 
 	grpsserv "github.com/AndrXxX/go-metrics-collector/internal/agent/services/grpc"
 	"github.com/AndrXxX/go-metrics-collector/internal/agent/services/grpc/dealoptions"
 	"github.com/AndrXxX/go-metrics-collector/internal/agent/services/metricsuploader"
+	"github.com/AndrXxX/go-metrics-collector/internal/agent/services/tlsconfig"
+	"github.com/AndrXxX/go-metrics-collector/internal/agent/types"
+	"github.com/AndrXxX/go-metrics-collector/internal/services/logger"
 )
 
 func WithGRPCMetricsUploader(hg hashGenerator) Option {
@@ -15,12 +20,20 @@ func WithGRPCMetricsUploader(hg hashGenerator) Option {
 		if a.c.Common.GRPCHost == "" {
 			return
 		}
-		opts := []grpc.DialOption{
-			grpc.WithTransportCredentials(insecure.NewCredentials()),
-			grpc.WithDefaultCallOptions(grpc.UseCompressor(gzip.Name)),
-			grpc.WithUnaryInterceptor(dealoptions.WithXRealIP(a.c.Common.Host)),
-			grpc.WithUnaryInterceptor(dealoptions.WithSHA256(hg, a.c.Common.Key)),
+		var opts types.ItemsList[grpc.DialOption]
+
+		tlsConf, err := tlsconfig.Provider{CryptoKeyPath: a.c.Common.CryptoKey}.Fetch()
+		if err != nil {
+			logger.Log.Error("failed to fetch tlsConf", zap.Error(err))
 		}
+		if tlsConf != nil {
+			opts.Add(grpc.WithTransportCredentials(credentials.NewTLS(tlsConf)))
+		} else {
+			opts.Add(grpc.WithTransportCredentials(insecure.NewCredentials()))
+		}
+		opts.Add(grpc.WithDefaultCallOptions(grpc.UseCompressor(gzip.Name)))
+		opts.Add(grpc.WithUnaryInterceptor(dealoptions.WithXRealIP(a.c.Common.Host)))
+		opts.Add(grpc.WithUnaryInterceptor(dealoptions.WithSHA256(hg, a.c.Common.Key)))
 		updater := grpsserv.NewGRPCMetricsUpdater(a.c.Common.GRPCHost, opts)
 		a.processors.Add(metricsuploader.NewGRPCUploader(updater))
 	}
